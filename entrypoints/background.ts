674f9a67c7badb76dcac75ed6a7aaf8bbb5f7660
helalsoft <helalsoft@proton.me>
2026-01-29 15:27:53 +0300

    feat: disable extension action on internal chrome:// pages via background script

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,18 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (tab.url?.startsWith('chrome://')) {
+      browser.action.disable(tabId);
+    } else {
+      browser.action.enable(tabId);
+    }
+  });
+
+  browser.tabs.onActivated.addListener(async (activeInfo) => {
+    const tab = await browser.tabs.get(activeInfo.tabId);
+    if (tab.url?.startsWith('chrome://')) {
+      browser.action.disable(activeInfo.tabId);
+    } else {
+      browser.action.enable(activeInfo.tabId);
+    }
+  });
 });
