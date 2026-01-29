18d96107d6a57f1480584bb1317a5d95be8fb333
helalsoft <helalsoft@proton.me>
2026-01-29 15:21:08 +0300

    fix: set extension name to 'Next Time' in manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
+    name: '__MSG_extension_name__',
     homepage_url: 'https://github.com/helalsoft/next-time',
     permissions: ['storage', 'tabs'],
     default_locale: 'en',
